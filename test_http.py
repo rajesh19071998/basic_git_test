4adf51bae5d8f222c3dd982c0cb1dfb1e4c2357e
Rajesh V <108512@grundfos.com>
2026-07-13 16:41:54 +0530

    test added5

--- test_http.py
+++ test_http.py
@@ -6,21 +6,24 @@ URL = "http://sw1.rajeshv.in/info"
 
 def test_status_code(extra):
     response = requests.get(URL)
-    assert response.status_code == 200, f"Expected 200, got {response.status_code}"
+    assert response.status_code == 200, f"❌ Expected 200, got {response.status_code}"
     extra.append(extras.text("✅ Status code check passed"))
 
 def test_response_not_empty(extra):
     response = requests.get(URL)
-    assert response.text.strip() != "", "Response body is empty"
+    assert response.text.strip() != "", "❌ Response body is empty"
     extra.append(extras.text("✅ Response body is not empty"))
 
 def test_response_board_num_match(extra):
     response = requests.get(URL)
-    assert "Board_Number : 5" in response.text, "❌ Response board number not 8"
+    assert "Board_Number : 5" in response.text, "❌ Response board number not 5"
     extra.append(extras.text("✅ Response body board number was 5"))
 
+    assert "Board_Name : SWITCH_BOARD_1" in response.text, "❌ Response Board_Name : SWITCH_BOARD_1 not found"
+    extra.append(extras.text("✅ Response body board name was SWITCH_BOARD_1"))
 
-def test_response_contains_board_number(extra):
-    response = requests.get(URL)
-    assert "Board_Number : 5" in response.text.lower(), "Response does not contain Board number = 5"
-    extra.append(extras.text("✅ Response contains Board number 5"))
+    assert "Board Local DNS : http://switch_board_1.local" in response.text, "❌ Response Board Local DNS not found"
+    extra.append(extras.text("✅ Response body board local DNS was http://switch_board_1.local"))
+
+
+
